5c423a5cd82aa4481b24ae564c5d9f8a04d1e917
Raeanna Bektashi <raeannabektashi@Raeannas-MacBook-Air.local>
2020-10-19 17:39:52 -0400

    Finished Lab

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let firstName = "Mitch";
+const name = "Mitch";
 const age = 25;
 const birthday = "Janurary 24";
 const detroitGC = true;
@@ -14,5 +14,30 @@ if (detroitGC === true) {
     `My name is ${name} and I am a student at Grand Circus in Detroit, Michigan. I am currently ${age} years old and my birthday is on ${birthday}`
   );
 } else {
-  // block of code to be executed if the condition is false
+  console.log(
+    `my name is ${name} and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently ${age} years old and my birthday is on ${birthday}`
+  );
+}
+
+for (let i = 0; i < lifeEvents.length; i++) {
+  console.log(lifeEvents[i]);
+}
+
+let counter = 0;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+while (true) {
+  const randomNumber = getRandomInt(10);
+  counter++;
+  if (randomNumber !== 5) {
+    console.log(`${randomNumber} !== 5`);
+  } else {
+    console.log(
+      `5 === 5. It took ${counter} iterations to randomly generate the number 5.`
+    );
+    break;
+  }
 }
