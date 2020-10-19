ada9e2e559d18e4ed94b0c75f60993eb965f3461
Raeanna Bektashi <raeannabektashi@Raeannas-MacBook-Air.local>
2020-10-19 16:23:51 -0400

    created repo

---
+++ script.js
@@ -0,0 +1,18 @@
+let firstName = "Mitch";
+const age = 25;
+const birthday = "Janurary 24";
+const detroitGC = true;
+let lifeEvents = [
+  "I was born in Troy, Michigan",
+  "I went to Hope College",
+  "I went to junior Olympics when I was 10 years old. I placed 14th in the nation in the 800 meter event.",
+  "I'm a graduate of the front-end bootcamp.",
+];
+
+if (detroitGC === true) {
+  console.log(
+    `My name is ${name} and I am a student at Grand Circus in Detroit, Michigan. I am currently ${age} years old and my birthday is on ${birthday}`
+  );
+} else {
+  // block of code to be executed if the condition is false
+}
